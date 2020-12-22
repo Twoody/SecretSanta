d86b6618f29b5fbf137bf4cdd1df01cef17188b2
Tanner Woody <tanner.l.woody@gmail.com>
2020-12-21 23:47:47 -0800

    init example file

---
+++ players-example.py
@@ -0,0 +1,24 @@
+'''
+A single file dedicated to the players of this secret santa game
+'''
+people = [
+    {
+        'full_name':    "Tanner Woody",
+        'email':        'Tanner.L.Woody@gmai.com',
+        'address':      '123 ABC St, Fa, KE, 12345',
+        'likes':        'Dogs, books, traveling, gardening, everything!'
+    },
+    (
+        'full_name':    'Alice',
+        'email':        'Tanner.L.Woody@gmai.com',
+        'address':      '1 Fake St, PDX, OR',
+        'likes':        'Stars'
+    ),
+    (
+        'full_name':    'Frank',
+        'email':        'Tanner.L.Woody@gmai.com',
+        'address':      '1 Sesame St, NYC, NY',
+        'likes':        'Math'
+    ),
+]
+
