98ac700004e1e31bc01216716a29b6d56faf5bdc
Tanner Woody <tanner.l.woody@gmail.com>
2020-12-22 01:51:25 -0800

    updated syntax

--- players-example.py
+++ players-example.py
@@ -8,17 +8,17 @@ people = [
         'address':      '123 ABC St, Fa, KE, 12345',
         'likes':        'Dogs, books, traveling, gardening, everything!'
     },
-    (
+    {
         'full_name':    'Alice',
         'email':        'Tanner.L.Woody@gmai.com',
         'address':      '1 Fake St, PDX, OR',
         'likes':        'Stars'
-    ),
-    (
+    },
+    {
         'full_name':    'Frank',
         'email':        'Tanner.L.Woody@gmai.com',
         'address':      '1 Sesame St, NYC, NY',
         'likes':        'Math'
-    ),
+    },
 ]
 
